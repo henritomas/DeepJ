c88aae27434001768f4ccc2e9686653e268223d4
henritomas <henri.tomas@eee.upd.edu.ph>
2019-05-09 02:37:43 +0800

    8/8

--- constants.py
+++ constants.py
@@ -34,7 +34,7 @@ MAX_NOTE = MIN_NOTE + NUM_OCTAVES * OCTAVE
 NUM_NOTES = MAX_NOTE - MIN_NOTE
 
 # Number of beats in a bar
-BEATS_PER_BAR = 4
+BEATS_PER_BAR = 8
 # Notes per quarter note
 NOTES_PER_BEAT = 8
 # The quickest note is a half-note
