c69b1638a68868d8577fd8ba10e597a05eab4c97
henritomas <henri.tomas@eee.upd.edu.ph>
2019-05-09 02:44:30 +0800

    8/4

--- constants.py
+++ constants.py
@@ -36,12 +36,12 @@ NUM_NOTES = MAX_NOTE - MIN_NOTE
 # Number of beats in a bar
 BEATS_PER_BAR = 8
 # Notes per quarter note
-NOTES_PER_BEAT = 8
+NOTES_PER_BEAT = 4
 # The quickest note is a half-note
 NOTES_PER_BAR = NOTES_PER_BEAT * BEATS_PER_BAR
 
 # Training parameters
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 SEQ_LEN = 8 * NOTES_PER_BAR
 
 # Hyper Parameters
